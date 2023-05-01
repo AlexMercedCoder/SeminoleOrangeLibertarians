e49af9b94231296dbc02bba6f03add49dd42429b
Alex Merced <alex@alexmerced.dev>
2023-04-30 21:53:02 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import GetInvolved from './components/GetInvolved';
+import Events from './components/Events';
+import Donate from './components/Donate';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <About/>
+      <GetInvolved/>
+      <Events/>
+      <Donate/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
